10d00bcdd64d296731b19d9db1838c37d87c7495
Zdravko Pandžić <Zdravko Pandzic>
2023-11-11 17:12:43 +0100

    manji file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
-import 'package:excel/excel.dart';
-import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:excel/excel.dart';
+import 'dart:io';
 
 void main() {
   runApp(MyApp());
@@ -10,39 +11,52 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Odaberi i učitaj Excel'),
-        ),
-        body: Center(
-          child: ElevatedButton(
-            onPressed: () async {
-              FilePickerResult? result = await FilePicker.platform.pickFiles(
-                type: FileType.custom,
-                allowedExtensions: ['xlsx', 'xls'],
-              );
-              print(result!.files.first.name);
+      home: HomePage(),
+    );
+  }
+}
 
-              if (result != null) {
-                var bytes = result.files.first.bytes;
-                print(result.files.first.bytes);
-                var excel = Excel.decodeBytes(bytes!);
+class HomePage extends StatelessWidget {
+  // Function to pick and read Excel file
+  void pickAndReadExcel() async {
+    // Pick the file
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.custom,
+      allowedExtensions: ['xlsx', 'xls'],
+    );
 
-                for (var table in excel.tables.keys) {
-                  print(table); // Ime sheet-a
-                  // print(excel.tables[table]?.maxCols);
-                  print(excel.tables[table]?.maxRows);
-                  for (var row in excel.tables[table]!.rows) {
-                    print("$row");
-                  }
-                }
-              } else {
-                // Korisnik je odustao od biranja datoteke
-                print("Odabir datoteke otkazan.");
-              }
-            },
-            child: Text('Odaberi Excel Datoteku'),
-          ),
+    if (result != null) {
+      File file = File(result.files.single.path!);
+
+      // Read the Excel file
+      var bytes = file.readAsBytesSync();
+      var excel = Excel.decodeBytes(bytes);
+
+      // Print the contents of the Excel file
+      for (var table in excel.tables.keys) {
+        print(table); //sheet Name
+        // print(excel.tables[table]?.maxCols);
+        print(excel.tables[table]?.maxRows);
+        for (var row in excel.tables[table]!.rows) {
+          print("$row");
+        }
+      }
+    } else {
+      // User canceled the picker
+      print('No file selected');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Excel Importer'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: pickAndReadExcel,
+          child: Text('Pick and Read Excel File'),
         ),
       ),
     );
